d3e06de8d2bf68245a4903668f45409cd16f7123
Dmytro Borysenkov <dmytroborysenkov@gmail.com>
2024-06-04 15:27:05 +0200

    Added extended onset and tempo evaluation plots ('--extended' option)

--- evaluate.py
+++ evaluate.py
@@ -6,7 +6,8 @@ Evaluates onset, beat and/or tempo predictions against ground truth.
 
 For usage information, call with --help.
 
-Author: Jan Schlüter
+Author of the skeleton: Jan Schlüter
+Author: Dmytro Borysenkov
 """
 
 import sys
@@ -18,6 +19,7 @@ from collections import defaultdict
 
 import numpy as np
 import mir_eval
+import matplotlib.pyplot as plt
 
 
 def opts_parser():
@@ -32,6 +34,10 @@ def opts_parser():
     parser.add_argument('predictions',
             type=str,
             help='The predictions directory of .pr files or a JSON file.')
+    parser.add_argument('--extended',
+            action='store_true',
+            default=False,
+            help="Enables detailed prints of evaluation")
     return parser
 
 
@@ -58,17 +64,61 @@ def read_data(path, extension='.gt'):
         return data
 
 
-def eval_onsets(truth, preds):
+def eval_onsets(truth, preds, options):
     """
     Computes the average onset detection F-score.
     """
-    return sum(mir_eval.onset.f_measure(np.asarray(truth[k]['onsets']),
-                                        np.asarray(preds[k]['onsets']),
-                                        0.05)[0]
-               for k in truth if k in preds) / len(truth)
+    onset_evals = dict()
+    for k in truth:
+        if k in preds:
+            onset_evals[k] = mir_eval.onset.f_measure(
+                                np.asarray(truth[k]['onsets']),
+                                np.asarray(preds[k]['onsets']),
+                                0.05)
+    
+    f_scores = [onset_evals[k][0] for k in onset_evals]
+
+    if options.extended:
+        bad_estimates = dict()
+        f_score_avg = np.mean(f_scores)
+        f_score_variance = np.var(f_scores)
+        for k in onset_evals:
+            if onset_evals[k][0] < f_score_avg - f_score_variance:
+                bad_estimates[k] = onset_evals[k]
+
+        bad_f_scores = [onset_evals[k][0] for k in bad_estimates]
+        bad_precisions = [onset_evals[k][1] for k in bad_estimates]
+        bad_recalls = [onset_evals[k][2] for k in bad_estimates]
+
+        bad_estimates_labels = [k if len(k)< 25 else f"{k[:10]}...{k[-10:]}" for k in list(bad_estimates.keys())]
+        
+        fig, ax = plt.subplots(1, 1, figsize=(12, 7), sharex=True, sharey=True)
+        ax.bar(bad_estimates_labels, bad_f_scores, color="orange", label='F-score')
+        ax.set_ylabel("f_score")
+        
+        ax.plot(bad_estimates_labels, bad_precisions, color="red", marker='x', linestyle='', label='Precision')
+        ax.set_ylabel("precision")
+
+        ax.plot(bad_estimates_labels, bad_recalls, color="blue", marker='o', linestyle='', label='Recall')
+        ax.set_ylabel("recall")
+
+        ax.set_xlim(-1, len(bad_estimates) + 1)
+        ax.set_ylim(0, 1.15)
+        ax.axhline(1, -1, len(bad_estimates_labels) + 2, color='black', linestyle='--')
+        ax.set_xticks(range(len(bad_estimates_labels)))
+        ax.set_xticklabels(bad_estimates_labels, rotation=90, ha='center')
+        plt.legend(loc='upper center', ncol=3, prop={'size': 14})
+        fig.suptitle(f"Onsets Estimation Extended Eval Plot of the worst onsets f-score  samples (total: {len(bad_estimates)})")
+        fig.tight_layout()
+        plt.show()
+
+        print(f"Poorly-guessed onsets of: {[k for k in bad_estimates]}")
+        print()
+
+    return sum(f_scores) / len(truth)
 
 
-def eval_tempo(truth, preds):
+def eval_tempo(truth, preds, options):
     """
     Computes the average tempo estimation p-score.
     """
@@ -83,14 +133,69 @@ def eval_tempo(truth, preds):
         if len(tempi) < 2:
             tempi = [tempi[0] / 2., tempi[0]]
         return np.asarray(tempi)
+    
+    prepared_preds = dict()
+    prepared_truth = dict()
+    prepared_weights = dict()
+    
+    tempo_evals = dict()
+    for k in truth:
+        if k in preds:
+            prepared_preds[k] = prepare_preds(preds[k]['tempo'])
+            prepared_truth[k], prepared_weights[k] = prepare_truth(truth[k]['tempo'])
+            tempo_evals[k] = mir_eval.tempo.detection(
+                                    prepared_truth[k], 
+                                    prepared_weights[k],
+                                    prepared_preds[k],
+                                    0.08)
+    p_scores = [tempo_evals[k][0] for k in tempo_evals]
 
-    return sum(mir_eval.tempo.detection(*prepare_truth(truth[k]['tempo']),
-                                        prepare_preds(preds[k]['tempo']),
-                                        0.08)[0]
-               for k in truth if k in preds) / len(truth)
+    if options.extended:
+        bad_estimates = dict()
+        worst_estimates = dict()
+        p_score_avg = np.mean(p_scores)
+        for k in tempo_evals:
+            if tempo_evals[k][1] < p_score_avg and not tempo_evals[k][2]:
+                    if tempo_evals[k][0] > 0:
+                        bad_estimates[k] = tempo_evals[k]
+                    else:
+                        worst_estimates[k] = (prepared_truth[k][0] - prepared_preds[k][0]) / prepared_truth[k][0]
+        
+        bad_p_scores = [tempo_evals[k][0] for k in bad_estimates]
+        
+        bad_estimates_labels = [k if len(k)< 25 else f"{k[:10]}...{k[-10:]}" for k in list(bad_estimates.keys())]
+        worst_estimates_labels = [k if len(k)< 25 else f"{k[:10]}...{k[-10:]}" for k in list(worst_estimates.keys())]
+        
+        fig, axs = plt.subplots(2, 1, figsize=(12, 7))
+        axs[0].bar(bad_estimates_labels, bad_p_scores, color="orange")
+        axs[0].set_xticks(range(len(bad_estimates_labels)))
+        axs[0].set_xticklabels(bad_estimates_labels, rotation=90, ha='center')
+        axs[0].set_ylabel("p_score")
+        axs[0].set_title(f"p-scores of 'half-guessed' (total: {len(bad_estimates)}) samples")
+        axs[0].set_xlim(-1, len(bad_estimates) + 1)
+        axs[0].set_ylim(0, 1)
+
+        axs[1].bar(worst_estimates_labels, 
+                   [abs(worst_estimates[k]) for k in worst_estimates], 
+                   color=["red" if worst_estimates[k] > 0 else "blue" for k in worst_estimates])
+        axs[1].set_xticks(range(len(worst_estimates_labels)))
+        axs[1].set_xticklabels(worst_estimates_labels, rotation=90, ha='center')
+        axs[1].set_ylabel("(gt[0] - pr[0]) / gt[0]")
+        axs[1].set_title(f"Relative errors of misspredicted (p_score = 0) samples (in total: {len(worst_estimates)}). Overestimated - red, under- blue.")
+        axs[1].set_xlim(-1, len(worst_estimates) + 1)
+        fig.suptitle("Tempo Estimation Extended Eval Plot")
+        fig.tight_layout()
+        plt.show()
+
+        print(f"Half-guessed: {[k for k in bad_estimates]}")
+        print()
+        print(f"Misspredicted completely: {[k for k in worst_estimates]}")
+        print()
+
+    return sum(p_scores) / len(truth)
 
 
-def eval_beats(truth, preds):
+def eval_beats(truth, preds, options):
     """
     Computes the average beat detection F-score.
     """
@@ -113,17 +218,20 @@ def main():
 
     # evaluate
     try:
-        print('Onsets F-score: %.4f' % eval_onsets(truth, preds))
+        print(f"Onsets F-score: {eval_onsets(truth, preds, options): .4f}")
+        print("-" * 20)
     except KeyError:
-        print('Onsets seemingly not included.')
+        print("Onsets seemingly not included.")
     try:
-        print('Tempo p-score: %.4f' % eval_tempo(truth, preds))
+        print(f"Tempo p-score: {eval_tempo(truth, preds, options): .4f}")
+        print("-" * 20)
     except KeyError:
-        print('Tempo seemingly not included.')
+        print("Tempo seemingly not included.")
     try:
-        print('Beats F-score: %.4f' % eval_beats(truth, preds))
+        print(f"Beats F-score: {eval_beats(truth, preds, options): .4f}")
+        print("-" * 20)
     except KeyError:
-        print('Beats seemingly not included.')
+        print("Beats seemingly not included.")
 
 
 if __name__ == "__main__":
